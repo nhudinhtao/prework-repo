4db2d76011aca3120615f81394b8b23322bad45a
Tao Nhu <taonhu@TaoNhus-MacBook-Pro.local>
2016-10-04 10:58:49 +0700

    add search function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,7 @@ class ArticlesController < ApplicationController
   # GET /articles.json
   def index
     @articles = Article.all
+    @articles = Article.search(params[:search])
     @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
   end
 
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ActiveRecord::Base
+	def self.search(search)
+	  if search
+	    self.where('title LIKE ? OR body LIKE ? ', "%#{search}%", "%#{search}%")
+	  else
+	    self.all
+	  end
+	end
 end
