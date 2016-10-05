dbeb5b49c89cbc33fff3d8d346c59e92f04811f6
Tao Nhu <taonhu@TaoNhus-MacBook-Pro.local>
2016-10-05 23:33:48 +0700

    change tab to space

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,9 @@
 class Article < ActiveRecord::Base
-	def self.search(search)
-	  if search
-	    self.where('title LIKE ? OR body LIKE ? ', "%#{search}%", "%#{search}%")
-	  else
-	    self.all
-	  end
-	end
+  def self.search(search)
+    if search
+      self.where('title LIKE ? OR body LIKE ? ', "%#{search}%", "%#{search}%")
+    else
+      self.all
+    end
+  end
 end
