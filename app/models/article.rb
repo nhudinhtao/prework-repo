9d63101fcd35a4e4a8086737f1aff27c77e0346f
Tao Nhu <taonhu@TaoNhus-MacBook-Pro.local>
2016-10-05 23:59:09 +0700

    validate model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
+  validates :title, :presence => true
+  validates :body, :presence => true
   def self.search(search)
     if search
       self.where('title LIKE ? OR body LIKE ? ', "%#{search}%", "%#{search}%")
